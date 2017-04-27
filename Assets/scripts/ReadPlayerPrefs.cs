7fd024e2fe86f3f23e155cef16b724de7beaacc6
Priyank Jain <priyank.jain@caviumnetworks.com>
2017-04-26 23:32:46 -0700

    Read Player Prefs button is functional

---
+++ Assets/scripts/ReadPlayerPrefs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class ReadPlayerPrefs : MonoBehaviour {
+
+	public void ReadPreferences()
+	{
+		resultTextUI.text = "Hello World";
+	}
+
+	#region Members
+	[SerializeField]
+	private Text resultTextUI;
+	#endregion
+}
